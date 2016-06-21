4d46097a11ed62eb937219b8941003c6f2700968
Maxwell Mitchell <mmitchell@cloudflare.com>
2016-06-20 23:08:20 -0700

    add run.duration flag; print summary on quit/time running out

--- cmd/main.go
+++ cmd/main.go
@@ -1,41 +1,82 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/assumednormal/go-mm1"
 )
 
-func main() {
+type summary struct {
+	n  int64    // number of jobs completed
+	si float64  // sum of interarrival durations
+	ss float64  // sum of service durations
+	pj *mm1.Job // previous job
+}
+
+func (s *summary) update(j *mm1.Job) {
+	s.n++
+	s.si += j.EnterQueue.Sub(s.pj.EnterQueue).Seconds()
+	s.ss += j.EndService.Sub(j.StartService).Seconds()
+	s.pj = j
+}
+
+func (s *summary) String() string {
+	out := fmt.Sprintf("Number of jobs completed: %d Jobs", s.n)
+	out = fmt.Sprintf("%s\nMean Interarrival Duration: %0.2f Seconds", out,
+		s.si/float64(s.n))
+	out = fmt.Sprintf("%s\nMean Service Duration: %0.2f Seconds\n", out,
+		s.ss/float64(s.n))
+	return out
+}
+
+func run() int {
 	var lambda, mu float64
+	var runDur time.Duration
 	flag.Float64Var(&lambda, "arrival.rate", 0, "arrival rate, lambda")
 	flag.Float64Var(&mu, "service.rate", 0, "service rate, mu")
+	flag.DurationVar(&runDur, "run.duration", time.Duration(0), "amount of time to run simulation")
 	flag.Parse()
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-	queue, err := mm1.New(lambda, mu)
+	q, err := mm1.New(lambda, mu)
 	if err != nil {
-		panic(err)
+		os.Stderr.WriteString(fmt.Sprintf("%v\n", err))
+		return 1
 	}
-	queue.Start()
-	defer queue.Stop()
+	start := q.Start()
+	defer q.Stop()
 
-	enc := json.NewEncoder(os.Stdout)
+	end := time.NewTicker(runDur)
 
-	go func() {
-		for {
-			j := <-queue.OutChan
-			enc.Encode(j)
+	s := &summary{
+		n:  int64(0),
+		si: float64(0),
+		ss: float64(0),
+		pj: &mm1.Job{EnterQueue: start},
+	}
+
+	for {
+		select {
+		case j := <-q.DoneChan:
+			s.update(j)
+		case sig := <-sigs:
+			os.Stdout.WriteString(fmt.Sprintf("\nQuitting on signal: %v", sig))
+			os.Stdout.WriteString(s.String())
+			return 0
+		case <-end.C:
+			os.Stdout.WriteString(s.String())
+			return 0
 		}
-	}()
+	}
+}
 
-	sig := <-sigs
-	fmt.Printf("\nQuitting on signal: %v\n", sig)
+func main() {
+	os.Exit(run())
 }
