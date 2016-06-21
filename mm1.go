65fd23504fca2266976726941d39181580ce7f35
Maxwell Mitchell <mmitchell@cloudflare.com>
2016-06-20 23:07:37 -0700

    simplify: send completed jobs to caller immediately, no add'l processing

--- mm1.go
+++ mm1.go
@@ -8,16 +8,18 @@ import (
 	"time"
 )
 
-var errLambdaNonPositive = errors.New("arrival rate (lambda) must be positive")
-var errMuNotGreaterThanLambda = errors.New("service rate (mu) must be greate than arrival rate (lambda)")
+// ErrLambdaNonPositive occurs when the arrival rate (lambda) is not positive.
+var ErrLambdaNonPositive = errors.New("arrival rate (lambda) must be positive")
+
+// ErrMuNotGreaterThanLambda occurs when the service rate (mu) is not greater
+// than the arrival rate (lambda).
+var ErrMuNotGreaterThanLambda = errors.New("service rate (mu) must be greater than arrival rate (lambda)")
 
 // Job contains data on an individual job flowing through the queue.
 type Job struct {
-	EnterQueue      time.Time
-	StartService    time.Time
-	EndService      time.Time
-	WaitDuration    float64
-	ServiceDuration float64
+	EnterQueue   time.Time
+	StartService time.Time
+	EndService   time.Time
 }
 
 // MM1 represents an M/M/1 queue with arrival rate lambda and service rate mu.
@@ -25,8 +27,7 @@ type MM1 struct {
 	lambda   float64    // arrival rate
 	mu       float64    // service rate
 	jobChan  chan *Job  // channel for jobs
-	doneChan chan *Job  // channel for completed jobs
-	OutChan  chan *Job  // channel to send processed completed jobs to caller
+	DoneChan chan *Job  // channel for completed jobs
 	rng      *rand.Rand // random number generator
 	stopChan chan bool  // tell arrival and service goroutines to stop
 }
@@ -36,62 +37,58 @@ type MM1 struct {
 func New(lambda, mu float64) (*MM1, error) {
 	// check that lambda is positive
 	if lambda <= 0 {
-		return nil, errLambdaNonPositive
+		return nil, ErrLambdaNonPositive
 	}
 
 	// check that mu is greater than lambda
 	if mu <= lambda {
-		return nil, errMuNotGreaterThanLambda
+		return nil, ErrMuNotGreaterThanLambda
 	}
 
-	// set length of channel to expected queue length + 3 * sd of queue length, to
+	// set length of channel to expected queue length + 10 * sd of queue length, to
 	// be somewhat safe
 	rho := lambda / mu
 	expectedLength := rho / (1 - rho)
 	sdLength := math.Sqrt(rho / math.Pow(1-rho, 2))
-	chanLength := int64(expectedLength + 3*sdLength)
+	chanLength := int64(expectedLength + 10*sdLength)
 
 	return &MM1{
 		lambda:   lambda,
 		mu:       mu,
 		jobChan:  make(chan *Job, chanLength),
-		doneChan: make(chan *Job, chanLength),
-		OutChan:  make(chan *Job, chanLength),
+		DoneChan: make(chan *Job, chanLength),
 		rng:      rand.New(rand.NewSource(time.Now().UnixNano())),
-		stopChan: make(chan bool, 4),
+		stopChan: make(chan bool, 2),
 	}, nil
 }
 
-// Start starts the arrival process and servicing in separate goroutines and
-// json encodes completed jobs for the caller.
-func (q *MM1) Start() {
-	go q.arrivalProcessStart()
-	go q.serviceStart()
-	go q.processCompletedJobs()
+// Start starts the arrival process and servicing in separate goroutines.
+func (q *MM1) Start() time.Time {
+	go q.arrivalProcess()
+	go q.serviceProcess()
+	return time.Now()
 }
 
 // Stop closes job and done channels.
 func (q *MM1) Stop() {
-	// send four messages on stopChan to signal goroutines
-	q.stopChan <- true
-	q.stopChan <- true
+	// send two messages on stopChan to signal goroutines
 	q.stopChan <- true
 	q.stopChan <- true
 
 	// close channels in q
 	close(q.jobChan)
-	close(q.doneChan)
-	close(q.OutChan)
+	close(q.DoneChan)
 	close(q.stopChan)
 }
 
-func (q *MM1) arrivalProcessStart() {
+func (q *MM1) arrivalProcess() {
 	for {
 		select {
 		case <-q.stopChan:
 			runtime.Goexit()
 		default:
-			timer := time.NewTimer(time.Duration(int64(q.rng.ExpFloat64() / q.lambda * float64(1e9))))
+			dur := time.Duration(int64(q.rng.ExpFloat64() / q.lambda * float64(1e9)))
+			timer := time.NewTimer(dur)
 			<-timer.C
 			q.jobChan <- &Job{
 				EnterQueue: time.Now(),
@@ -100,31 +97,18 @@ func (q *MM1) arrivalProcessStart() {
 	}
 }
 
-func (q *MM1) processCompletedJobs() {
-	for {
-		select {
-		case j := <-q.doneChan:
-			j.WaitDuration = j.StartService.Sub(j.EnterQueue).Seconds()
-			j.ServiceDuration = j.EndService.Sub(j.StartService).Seconds()
-			q.OutChan <- j
-		case <-q.stopChan:
-			runtime.Goexit()
-		}
-	}
-}
-
-func (q *MM1) serviceStart() {
+func (q *MM1) serviceProcess() {
 	for {
 		select {
 		case <-q.stopChan:
 			runtime.Goexit()
-		default:
-			j := <-q.jobChan
+		case j := <-q.jobChan:
 			j.StartService = time.Now()
-			timer := time.NewTimer(time.Duration(int64(q.rng.ExpFloat64() / q.mu * float64(1e9))))
+			dur := time.Duration(int64(q.rng.ExpFloat64() / q.mu * float64(1e9)))
+			timer := time.NewTimer(dur)
 			<-timer.C
 			j.EndService = time.Now()
-			q.doneChan <- j
+			q.DoneChan <- j
 		}
 	}
 }
